9ecda2997c001b90a5e246bc2b2ac369a2e81d1a
unknown <Stefan@.(none)>
2010-03-07 23:25:00 +0100

    commit for pull from remote

--- NetBeansProject/x86InfoAndControlGUI_Windows/nbproject/Makefile-variables.mk
+++ NetBeansProject/x86InfoAndControlGUI_Windows/nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
 CND_ARTIFACT_NAME_Debug=x86infoandcontrolgui_windows
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/x86infoandcontrolgui_windows
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/x86infoandcontrolgui_windows
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
 CND_PACKAGE_NAME_Debug=x86infoandcontrolguiwindows.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/x86infoandcontrolguiwindows.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/x86infoandcontrolguiwindows.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
