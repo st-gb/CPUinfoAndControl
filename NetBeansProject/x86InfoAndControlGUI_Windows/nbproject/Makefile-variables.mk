903b84396e550187913064a426c3f264183e7855
Stefan Gebauer <steff@goexchange.de>
2010-10-25 00:51:00 +0200

    commit for pull from 720qm

--- NetBeansProject/x86InfoAndControlGUI_Windows/nbproject/Makefile-variables.mk
+++ NetBeansProject/x86InfoAndControlGUI_Windows/nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=MinGW-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
+CND_PLATFORM_Debug=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
 CND_ARTIFACT_NAME_Debug=x86infoandcontrolgui_windows
-CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/x86infoandcontrolgui_windows
-CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/x86infoandcontrolgui_windows
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
 CND_PACKAGE_NAME_Debug=x86infoandcontrolguiwindows.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/x86infoandcontrolguiwindows.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/x86infoandcontrolguiwindows.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
@@ -22,3 +22,11 @@ CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/x86infoandcontrolgui_w
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
 CND_PACKAGE_NAME_Release=x86infoandcontrolguiwindows.tar
 CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/x86infoandcontrolguiwindows.tar
+# MinGW_Debug configuration
+CND_PLATFORM_MinGW_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_MinGW_Debug=dist/MinGW_Debug/MinGW-Windows
+CND_ARTIFACT_NAME_MinGW_Debug=x86infoandcontrolgui_windows
+CND_ARTIFACT_PATH_MinGW_Debug=dist/MinGW_Debug/MinGW-Windows/x86infoandcontrolgui_windows
+CND_PACKAGE_DIR_MinGW_Debug=dist/MinGW_Debug/MinGW-Windows/package
+CND_PACKAGE_NAME_MinGW_Debug=x86infoandcontrolguiwindows.tar
+CND_PACKAGE_PATH_MinGW_Debug=dist/MinGW_Debug/MinGW-Windows/package/x86infoandcontrolguiwindows.tar
